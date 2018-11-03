9e21e5b887d81b1bc03999bb043bad96d9dcd0d7
= <=>
2018-11-03 18:53:39 +0000

    added db seeder

--- src/SportRanker.Tools.DatabaseSeed.App/Program.cs
+++ src/SportRanker.Tools.DatabaseSeed.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using SportRanker.Tools.DatabaseSeed.Application;
+using SportRanker.Tools.DatabaseSeed.Contracts;
 
 namespace SportRanker.Tools.DatabaseSeed.App
 {
@@ -7,6 +8,8 @@ namespace SportRanker.Tools.DatabaseSeed.App
     {
         static void Main(string[] args)
         {
+
+            var dbSeeder = new DatabaseSeeder();
 
             var states = StateReader.ParseCsv(
                 @"C:\Code\SportRanker\SportRanker.Tools.DatabaseSeed\src\SportRanker.Tools.DatabaseSeed.App\Data\states.csv");
@@ -19,6 +22,8 @@ namespace SportRanker.Tools.DatabaseSeed.App
 
             var cities = CityReader.ParseCsv(
                 @"C:\Code\SportRanker\SportRanker.Tools.DatabaseSeed\src\SportRanker.Tools.DatabaseSeed.App\Data\cities.csv");
+
+            //dbSeeder.SeedData<City>(cities, "cities").Wait();
 
             Console.ReadKey();
         }
---
+++ src/SportRanker.Tools.DatabaseSeed.Application/DatabaseSeeder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace SportRanker.Tools.DatabaseSeed.Application
+{
+    public class DatabaseSeeder
+    {
+        private readonly MongoClient _client;
+
+        private const string DbConnectionString = @"mongodb://dbadmin:D1s!r1but3d@ds048279.mlab.com:48279/sportranker";
+        private const string DbName = "sportranker";
+        
+        public DatabaseSeeder()
+        {
+            _client = new MongoClient(DbConnectionString);
+        }
+
+        public async Task SeedData<T>(ICollection<T> data, string collectionName)
+        {
+            var database = _client.GetDatabase(DbName);
+
+            var collection = database.GetCollection<T>(collectionName);
+
+            await collection.InsertManyAsync(data);
+        }
+    }
+}
